0a5569b9532642cf53f4e800c6a91a79479c2b27
iambhushan6 <bhushan.lokhande19@vit.edu>
2023-09-15 21:40:36 +0530

    fix

--- flagright/flagright/settings.py
+++ flagright/flagright/settings.py
@@ -17,8 +17,6 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-YOUTUBE_API_KEY = config('YOUTUBE_API_KEY')
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
